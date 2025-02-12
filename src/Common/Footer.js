fe1b8390072a4a57dd3d7cb9193b0af116b13a00
dp <codewithdp2073@gmail.com>
2025-02-12 19:00:28 +0530

    footer created

---
+++ src/Common/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+            <Main />
+        </ >
+    )
+}
+function Main() {
+    return (
+        <>
+            <footer>
+                <div class="pull-right">
+                    Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com/">Colorlib</a>
+                </div>
+                <div class="clearfix"></div>
+            </footer>
+        </>
+    );
+}
+
+export default Footer
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navigation from '../Common/Navigation';
 import Slider from '../Common/Slider';
+import Footer from '../Common/Footer';
 
 const Home = () => {
     return (
         <>
             <Slider />
             <Navigation />
+            <Footer />
         </>
     )
 }
