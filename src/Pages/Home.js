7e6d3b295ace49810aa9b5b0305d84565fd24763
dp <codewithdp2073@gmail.com>
2025-02-12 18:58:15 +0530

    Left-slider and navigation created

---
+++ src/Pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navigation from '../Common/Navigation';
+import Slider from '../Common/Slider';
+
+const Home = () => {
+    return (
+        <>
+            <Slider />
+            <Navigation />
+        </>
+    )
+}
+
+function Main() {
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default Home
